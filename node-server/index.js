0ecea19b1db217f78788cea3501618d6e021d5b2
Dhairya22 <dhairyashelar1@gmail.com>
2023-02-08 15:43:35 +0530

    Empty commit

---
+++ node-server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const { Server } = require('socket.io');
+const app = express();
+
+const server = app.listen(8000);
+const io = new Server(server, { cors: { origin: '*' } });
+const users = {};
+
+io.on('connection', socket => {
+    //  If any new user joins, let other users connected to the server know!
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    // If someone sends a message, broadcast it to other people!
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id]});
+    });
+
+    // If someone sends a message, let others know!
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+});
+
+module.exports.handler = serverless(app);
